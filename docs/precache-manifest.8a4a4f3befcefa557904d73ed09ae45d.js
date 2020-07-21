dcaffdafeff1ead8019171671cb56e2defd83ba7
007tom <zhyd007@gmail.com>
2020-07-21 18:15:45 +0800

    feat: added Highlightjs for latex display

---
+++ docs/precache-manifest.8a4a4f3befcefa557904d73ed09ae45d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11208af89b5f8d5867e66dd77a9c3789",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56d3341b998f934a22a2",
+    "url": "/static/css/2.7505c40e.chunk.css"
+  },
+  {
+    "revision": "4668b015d9a98fb25e2a",
+    "url": "/static/css/main.11595e24.chunk.css"
+  },
+  {
+    "revision": "56d3341b998f934a22a2",
+    "url": "/static/js/2.de6a458f.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/static/js/2.de6a458f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4668b015d9a98fb25e2a",
+    "url": "/static/js/main.5161c276.chunk.js"
+  },
+  {
+    "revision": "84e1131d4cf79b58c393",
+    "url": "/static/js/runtime-main.271cc93f.js"
+  }
+]);
--- src/components/RichTextEditor.js
+++ src/components/RichTextEditor.js
@@ -1,342 +1,361 @@
 import React from 'react';
-import { Editor, EditorState, getDefaultKeyBinding, RichUtils, convertToRaw } from 'draft-js';
+import {Editor, EditorState, getDefaultKeyBinding, RichUtils, convertToRaw} from 'draft-js';
 import './RichTextEditor.css';
 import '../../node_modules/draft-js/dist/Draft.css';
+import hljs from 'highlight.js'
+import 'highlight.js/styles/monokai.css'
 
 class RichTextEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.editorRef = React.createRef();
-    this.focus = () => this.editorRef.current.focus();
-    this.onChange = (editorState) => this.setState({editorState});
+	constructor(props) {
+		super(props);
+		this.state = {editorState: EditorState.createEmpty()};
+		this.editorRef = React.createRef();
+		this.focus = () => this.editorRef.current.focus();
+		this.onChange = (editorState) => this.setState({editorState});
 
-    this.handleKeyCommand = this._handleKeyCommand.bind(this);
-    this.mapKeyToEditorCommand = this._mapKeyToEditorCommand.bind(this);
-    this.toggleBlockType = this._toggleBlockType.bind(this);
-    this.toggleInlineStyle = this._toggleInlineStyle.bind(this);
-  }
+		this.handleKeyCommand = this._handleKeyCommand.bind(this);
+		this.mapKeyToEditorCommand = this._mapKeyToEditorCommand.bind(this);
+		this.toggleBlockType = this._toggleBlockType.bind(this);
+		this.toggleInlineStyle = this._toggleInlineStyle.bind(this);
+	}
 
-  _handleKeyCommand(command, editorState) {
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return true;
-    }
-    return false;
-  }
+	componentDidMount() {
+		this.highlightCallBack()
+	}
 
-  _mapKeyToEditorCommand(e) {
-    if (e.keyCode === 9 /* TAB */) {
-      const newEditorState = RichUtils.onTab(
-        e,
-        this.state.editorState,
-        4, /* maxDepth */
-      );
-      if (newEditorState !== this.state.editorState) {
-        this.onChange(newEditorState);
-      }
-      return;
-    }
-    return getDefaultKeyBinding(e);
-  }
+	componentDidUpdate() {
+		this.highlightCallBack()
+	}
 
-  _toggleBlockType(blockType) {
-    this.onChange(
-      RichUtils.toggleBlockType(
-        this.state.editorState,
-        blockType
-      )
-    );
-  }
+	highlightCallBack = () => {
+		document.querySelectorAll('pre code')
+			.forEach(block => hljs.highlightBlock(block))
+	}
 
-  _toggleInlineStyle(inlineStyle) {
-    this.onChange(
-      RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        inlineStyle
-      )
-    );
-  }
+	_handleKeyCommand(command, editorState) {
+		const newState = RichUtils.handleKeyCommand(editorState, command);
+		if (newState) {
+			this.onChange(newState);
+			return true;
+		}
+		return false;
+	}
 
-  _onDB() {
-    const contentState = this.state.editorState.getCurrentContent();
-    const editorContentRaw = convertToRaw(contentState);
-    console.log(editorContentRaw);
-    const DB = 'test';
+	_mapKeyToEditorCommand(e) {
+		if (e.keyCode === 9 /* TAB */) {
+			const newEditorState = RichUtils.onTab(
+				e,
+				this.state.editorState,
+				4, /* maxDepth */
+			);
+			if (newEditorState !== this.state.editorState) {
+				this.onChange(newEditorState);
+			}
+			return;
+		}
+		return getDefaultKeyBinding(e);
+	}
 
-    let DBDeleteRequest = window.indexedDB.deleteDatabase(DB);
+	_toggleBlockType(blockType) {
+		this.onChange(
+			RichUtils.toggleBlockType(
+				this.state.editorState,
+				blockType
+			)
+		);
+	}
 
-    DBDeleteRequest.onerror = function (event) {
-      console.log('Error deleting database.');
-    };
+	_toggleInlineStyle(inlineStyle) {
+		this.onChange(
+			RichUtils.toggleInlineStyle(
+				this.state.editorState,
+				inlineStyle
+			)
+		);
+	}
 
-    DBDeleteRequest.onsuccess = function (event) {
-      console.log('Database deleted successfully');
-      console.log(event.result);
-    };
+	_onDB() {
+		const contentState = this.state.editorState.getCurrentContent();
+		const editorContentRaw = convertToRaw(contentState);
+		console.log(editorContentRaw);
+		const DB = 'test';
 
-    let request = window.indexedDB.open(DB, 1);
-    request.onerror = function(event) {
-      alert("Error opening the database");
-    };
+		let DBDeleteRequest = window.indexedDB.deleteDatabase(DB);
 
-    request.onsuccess = function(event) {
-      console.log('hello');
-    }
+		DBDeleteRequest.onerror = function (event) {
+			console.log('Error deleting database.');
+		};
 
-    request.onupgradeneeded = function(event)
-    {
-      const db = event.target.result;
-      const objectStore = db.createObjectStore(`contents`, {keyPath: "order", autoIncrement: true});
-      objectStore.createIndex("order", "order", {unique: true});
-      for (let i = 0; i < editorContentRaw.blocks.length; i++) {
-        objectStore.put(editorContentRaw.blocks[i]);
-      }
-    };
-  }
+		DBDeleteRequest.onsuccess = function (event) {
+			console.log('Database deleted successfully');
+			console.log(event.result);
+		};
 
-  _toTeX() {
-    let db, dbReq = indexedDB.open('test', 1);
+		let request = window.indexedDB.open(DB, 1);
+		request.onerror = function (event) {
+			alert("Error opening the database");
+		};
 
-    dbReq.onsuccess = function (event) {
-      db = event.target.result;
-      console.log("open 'test' successfully!!!");
-      getAndDisplaysTeX(db);
-    }
+		request.onsuccess = function (event) {
+			console.log('indexedDB request is successful');
+		}
 
-    dbReq.onerror = function (event) {
-      alert('error opening database ' + event.target.errorCode);
-    }
+		request.onupgradeneeded = function (event) {
+			const db = event.target.result;
+			const objectStore = db.createObjectStore(`contents`, {keyPath: "order", autoIncrement: true});
+			objectStore.createIndex("order", "order", {unique: true});
+			for (let i = 0; i < editorContentRaw.blocks.length; i++) {
+				objectStore.put(editorContentRaw.blocks[i]);
+			}
+		};
+	}
 
-    function getAndDisplaysTeX(db) {
-      let tx = db.transaction(['contents'], 'readonly');
-      let store = tx.objectStore('contents');
+	_toTeX() {
+		let db, dbReq = indexedDB.open('test', 1);
 
-      let req = store.openCursor();
-      let allTeX = [];
+		dbReq.onsuccess = function (event) {
+			db = event.target.result;
+			console.log("open 'test' indexedDB successfully!!!");
+			getAndDisplaysTeX(db);
+		}
 
-      req.onsuccess = function (event) {
-        let cursor = event.target.result;
-        let offset = 0;
+		dbReq.onerror = function (event) {
+			alert('error opening database ' + event.target.errorCode);
+		}
 
-        if (cursor != null) {
-          // allTeX.push(cursor.value.text);
-          let someTeX = cursor.value;
-          // console.log(someTeX);
-          let oSort = [], someTeXInlineStyleSort = [];
+		function getAndDisplaysTeX(db) {
+			let tx = db.transaction(['contents'], 'readonly');
+			let store = tx.objectStore('contents');
 
-          for (let i = 0; i < someTeX.inlineStyleRanges.length; i++) {
-            let o = someTeX.inlineStyleRanges[i].offset;
-            oSort.push(o);
-          }
-          oSort.sort((a, b) => a - b);
+			let req = store.openCursor();
+			let allTeX = [];
 
-          for (let i = 0; i < oSort.length; i++) {
-            for (let item in someTeX.inlineStyleRanges) {
-              if (someTeX.inlineStyleRanges[item].offset === oSort[i]) {
-                someTeXInlineStyleSort.push(someTeX.inlineStyleRanges[item]);
-              }
-            }
-          }
+			req.onsuccess = function (event) {
+				let cursor = event.target.result;
+				let offset = 0;
 
-          // console.log(someTeXInlineStyleSort);
+				if (cursor != null) {
+					// allTeX.push(cursor.value.text);
+					let someTeX = cursor.value;
+					// console.log(someTeX);
+					let oSort = [], someTeXInlineStyleSort = [];
 
-          if (someTeX.inlineStyleRanges.length === 0) {
-            allTeX.push(texMap[someTeX.type] + '{' + someTeX.text + '}<br/>');
-          } else {
-            for (let i = 0; i < someTeXInlineStyleSort.length; i++) {
-              let x = oSort[i];
-              let p = someTeXInlineStyleSort[i].length;
-              let q = someTeXInlineStyleSort[i].style;
+					for (let i = 0; i < someTeX.inlineStyleRanges.length; i++) {
+						let o = someTeX.inlineStyleRanges[i].offset;
+						oSort.push(o);
+					}
+					oSort.sort((a, b) => a - b);
 
-              if (i === 0) {
-                allTeX.push(someTeX.text.slice(0, x));
-              } else {
-                allTeX.push(someTeX.text.slice(offset+1, x));
-              }
-              allTeX.push(texMap[q] + '{' + someTeX.text.slice(x, x+p) + '}');
+					for (let i = 0; i < oSort.length; i++) {
+						for (let item in someTeX.inlineStyleRanges) {
+							if (someTeX.inlineStyleRanges[item].offset === oSort[i]) {
+								someTeXInlineStyleSort.push(someTeX.inlineStyleRanges[item]);
+							}
+						}
+					}
 
-              if (i === someTeXInlineStyleSort.length-1) {
-                allTeX.push(someTeX.text.slice(-(someTeX.text.length - x - p)) + '<br/>');
-              }
-              offset = x;
-            }
-          }
+					if (someTeX.inlineStyleRanges.length === 0) {
+						allTeX.push(texMap[someTeX.type] + '{' + someTeX.text + '}<br/>');
+					} else {
+						for (let i = 0; i < someTeXInlineStyleSort.length; i++) {
+							let x = oSort[i];
+							let p = someTeXInlineStyleSort[i].length;
+							let q = someTeXInlineStyleSort[i].style;
 
-          cursor.continue();
-        } else {
-          displayTeX(allTeX);
-        }
-      }
+							if (i === 0) {
+								allTeX.push(someTeX.text.slice(0, x));
+							} else {
+								allTeX.push(someTeX.text.slice(offset+1, x));
+							}
+							allTeX.push(texMap[q] + '{' + someTeX.text.slice(x, x+p) + '}');
 
-      req.onerror = function (event) {
-        alert('error in cursor request ' + event.target.errorCode);
-      }
-    }
+							if (i === someTeXInlineStyleSort.length-1) {
+								allTeX.push(someTeX.text.slice(-(someTeX.text.length - x - p)) + '<br/>');
+							}
+							offset = x;
+						}
+					}
 
-    function displayTeX(tex) {
-      let listHTML = '<pre><code class="latex">';
-      for (let i = 0; i < tex.length; i++) {
-        let note = tex[i];
-        listHTML += note;
-      }
-      listHTML += '</code></pre>';
-      document.getElementById('tex').innerHTML = listHTML;
-    }
-  }
+				cursor.continue();
+				} else {
+					displayTeX(allTeX);
+				}
+		}
+
+		req.onerror = function (event) {
+			alert('error in cursor request ' + event.target.errorCode);
+		}
+	}
+
+	function displayTeX(tex) {
+		let listHTML = '<pre><code class="latex">';
+		for (let i = 0; i < tex.length; i++) {
+			let note = tex[i];
+			listHTML += note;
+		}
+		listHTML += '</code></pre>';
+		document.getElementById('tex').innerHTML = listHTML;
+	}
+}
 
   render() {
     const {editorState} = this.state;
 
-    // If the user changes block type before entering any text, we can
-    // either style the placeholder or hide it. Let's just hide it now.
-    let className = 'RichEditor-editor';
-    let contentState = editorState.getCurrentContent();
-    if (!contentState.hasText()) {
-      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
-        className += ' RichEditor-hidePlaceholder';
-      }
-    }
+	// If the user changes block type before entering any text, we can
+	// either style the placeholder or hide it. Let's just hide it now.
+	let className = 'RichEditor-editor';
+	let contentState = editorState.getCurrentContent();
+	if (!contentState.hasText()) {
+		if (contentState.getBlockMap().first().getType() !== 'unstyled') {
+			className += ' RichEditor-hidePlaceholder';
+		}
+	}
 
-    return (
-      <div className="RichEditor-root">
-        <button onClick={this._onDB.bind(this)}>SaveData</button>
-        <br/>
-        <button onClick={this._toTeX.bind(this)}>TeX</button>
-        <BlockStyleControls
-          editorState={editorState}
-          onToggle={this.toggleBlockType}
-        />
-        <InlineStyleControls
-          editorState={editorState}
-          onToggle={this.toggleInlineStyle}
-        />
-        <div className={className} onClick={this.focus}>
-          <Editor
-            blockStyleFn={getBlockStyle}
-            customStyleMap={styleMap}
-            editorState={editorState}
-            handleKeyCommand={this.handleKeyCommand}
-            keyBindingFn={this.mapKeyToEditorCommand}
-            onChange={this.onChange}
-            placeholder="Tell a story..."
-            ref={this.editorRef}
-            spellCheck={true}
-          />
-        </div>
-        <div id={'tex'}></div>
-      </div>
-    );
-  }
+	return (
+		<div>
+			<div className="RichEditor-root">
+				<button onClick={this._onDB.bind(this)}>Save</button>&nbsp;
+				<button onClick={this._toTeX.bind(this)}>Display</button>
+				<BlockStyleControls
+					editorState={editorState}
+					onToggle={this.toggleBlockType}
+				/>
+				<InlineStyleControls
+					editorState={editorState}
+					onToggle={this.toggleInlineStyle}
+				/>
+				<div className={className} onClick={this.focus}>
+					<Editor
+						blockStyleFn={getBlockStyle}
+						customStyleMap={styleMap}
+						editorState={editorState}
+						handleKeyCommand={this.handleKeyCommand}
+						keyBindingFn={this.mapKeyToEditorCommand}
+						onChange={this.onChange}
+						placeholder="Tell a story..."
+						ref={this.editorRef}
+						spellCheck={true}
+					/>
+				</div>
+			</div>
+			<div
+				id='tex'
+			></div>
+		</div>
+	);
+}
 }
 
 const texMap = {
-  'header-two': '\\subsection',
-  'BOLD': '\\textbf',
-  'ITALIC': '\\textit',
-  'header-four': '\\subsubsubsection'
+	'header-one': '\\section',
+	'header-two': '\\subsection',
+	'header-three': '\\subsubsection',
+	'header-four': '\\subsubsubsection',
+	'BOLD': '\\textbf',
+	'ITALIC': '\\textit',
 };
 
 // Custom overrides for "code" style.
 const styleMap = {
-  CODE: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 2,
-  },
+	CODE: {
+		backgroundColor: 'rgba(0, 0, 0, 0.05)',
+		fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+		fontSize: 16,
+		padding: 2,
+	},
 };
 
 function getBlockStyle(block) {
-  switch (block.getType()) {
-    case 'blockquote': return 'RichEditor-blockquote';
-    default: return null;
-  }
+	switch (block.getType()) {
+		case 'blockquote':
+			return 'RichEditor-blockquote';
+		default:
+			return null;
+	}
 }
 
 class StyleButton extends React.Component {
-  constructor() {
-    super();
-    this.onToggle = (e) => {
-      e.preventDefault();
-      this.props.onToggle(this.props.style);
-    };
-  }
+	constructor() {
+		super();
+		this.onToggle = (e) => {
+			e.preventDefault();
+			this.props.onToggle(this.props.style);
+		};
+	}
 
-  render() {
-    let className = 'RichEditor-styleButton';
-    if (this.props.active) {
-      className += ' RichEditor-activeButton';
-    }
+	render() {
+		let className = 'RichEditor-styleButton';
+		if (this.props.active) {
+			className += ' RichEditor-activeButton';
+		}
 
-    return (
-      <span className={className} onMouseDown={this.onToggle}>
+		return (
+			<span className={className} onMouseDown={this.onToggle}>
         {this.props.label}
       </span>
-    );
-  }
+		);
+	}
 }
 
 const BLOCK_TYPES = [
-  {label: 'H1', style: 'header-one'},
-  {label: 'H2', style: 'header-two'},
-  {label: 'H3', style: 'header-three'},
-  {label: 'H4', style: 'header-four'},
-  {label: 'H5', style: 'header-five'},
-  {label: 'H6', style: 'header-six'},
-  {label: 'Blockquote', style: 'blockquote'},
-  {label: 'UL', style: 'unordered-list-item'},
-  {label: 'OL', style: 'ordered-list-item'},
-  {label: 'Code Block', style: 'code-block'},
+	{label: 'H1', style: 'header-one'},
+	{label: 'H2', style: 'header-two'},
+	{label: 'H3', style: 'header-three'},
+	{label: 'H4', style: 'header-four'},
+	{label: 'H5', style: 'header-five'},
+	{label: 'H6', style: 'header-six'},
+	{label: 'Blockquote', style: 'blockquote'},
+	{label: 'UL', style: 'unordered-list-item'},
+	{label: 'OL', style: 'ordered-list-item'},
+	{label: 'Code Block', style: 'code-block'},
 ];
 
 const BlockStyleControls = (props) => {
-  const {editorState} = props;
-  const selection = editorState.getSelection();
-  const blockType = editorState
-    .getCurrentContent()
-    .getBlockForKey(selection.getStartKey())
-    .getType();
+	const {editorState} = props;
+	const selection = editorState.getSelection();
+	const blockType = editorState
+		.getCurrentContent()
+		.getBlockForKey(selection.getStartKey())
+		.getType();
 
-  return (
-    <div className="RichEditor-controls">
-      {BLOCK_TYPES.map((type) =>
-        <StyleButton
-          key={type.label}
-          active={type.style === blockType}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  );
+	return (
+		<div className="RichEditor-controls">
+			{BLOCK_TYPES.map((type) =>
+				<StyleButton
+					key={type.label}
+					active={type.style === blockType}
+					label={type.label}
+					onToggle={props.onToggle}
+					style={type.style}
+				/>
+			)}
+		</div>
+	);
 };
 
 const INLINE_STYLES = [
-  {label: 'Bold', style: 'BOLD'},
-  {label: 'Italic', style: 'ITALIC'},
-  {label: 'Underline', style: 'UNDERLINE'},
-  {label: 'Monospace', style: 'CODE'},
+	{label: 'Bold', style: 'BOLD'},
+	{label: 'Italic', style: 'ITALIC'},
+	{label: 'Underline', style: 'UNDERLINE'},
+	{label: 'Monospace', style: 'CODE'},
 ];
 
 const InlineStyleControls = (props) => {
-  const currentStyle = props.editorState.getCurrentInlineStyle();
-  
-  return (
-    <div className="RichEditor-controls">
-      {INLINE_STYLES.map((type) =>
-        <StyleButton
-          key={type.label}
-          active={currentStyle.has(type.style)}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  );
+	const currentStyle = props.editorState.getCurrentInlineStyle();
+
+	return (
+		<div className="RichEditor-controls">
+			{INLINE_STYLES.map((type) =>
+				<StyleButton
+					key={type.label}
+					active={currentStyle.has(type.style)}
+					label={type.label}
+					onToggle={props.onToggle}
+					style={type.style}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default RichTextEditor;
