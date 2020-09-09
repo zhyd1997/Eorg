097a1d51e1ed31261cbd5589623aa7723c9c024a
007tom <zhyd007@gmail.com>
2020-09-09 18:11:00 +0800

    refactor: deleted useless Highlight.js

--- src/components/Highlight.js
+++
@@ -1,9 +0,0 @@
-import hljs from 'highlight.js'
-import 'highlight.js/styles/zenburn.css'
-
-const highlightCallBack = () => {
-	document.querySelectorAll('pre code')
-		.forEach((block) => hljs.highlightBlock(block))
-}
-
-export default highlightCallBack
