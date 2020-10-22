85bfd321a247b38299647504a3e8f9981009adb2
007tom <zhyd007@gmail.com>
2020-10-22 22:55:44 +0800

    build: added jest for unit-test

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	rootDir: './src',
+	roots: ['<rootDir>'],
+	transform: {
+		'^.+\\.tsx?$': 'ts-jest',
+	},
+	setupFilesAfterEnv: [
+		'./setupTests.ts',
+	],
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
