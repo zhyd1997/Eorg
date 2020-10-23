06d4498282c059232f28e4aa4bab650fb98c9f9a
007tom <zhyd007@gmail.com>
2020-10-23 13:56:27 +0800

    refactor: code splitting of Editor Component

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -16,6 +16,9 @@ import ModalExample from '../Zotero/ModalExample'
 import './BlockComponent/TeX/TeXEditor.css'
 import './BlockComponent/Table/Table.css'
 import './BlockComponent/Image/imageBlockStyle.css'
+import {
+	InlineStyleControls, BlockStyleControls, styleMap, getBlockStyle,
+} from './utils'
 
 /**
  * Editor Template and KaTeX support are all referenced to Draft.js official example.
@@ -29,126 +32,11 @@ import './BlockComponent/Image/imageBlockStyle.css'
  *
  */
 
-type StyleButtonTypes = {
-	onToggle: (style: string) => void,
-	style: string,
-	active: boolean,
-	label: string,
-}
-
-type StyleControlsTypes = {
-	editorState: EditorState,
-	onToggle: (style: string) => void,
-}
-
 type RichTextEditorTypes = {
 	login: boolean,
 	store: {
 		token: string,
 	},
-}
-
-const StyleButton: React.FC<StyleButtonTypes> = ({
-	onToggle, style, active, label,
-}) => {
-// @ts-expect-error ts-migrate(7006) FIXME: Parameter 'e' implicitly has an 'any' type.
-	function onToggleStyle(e): void {
-		e.preventDefault()
-		onToggle(style)
-	}
-	let className = 'RichEditor-styleButton'
-	if (active) {
-		className += ' RichEditor-activeButton'
-	}
-
-	return (
-		// eslint-disable-next-line jsx-a11y/no-static-element-interactions
-		<span className={className} onMouseDown={onToggleStyle}>
-			{label}
-		</span>
-	)
-}
-
-const INLINE_STYLES = [
-	{ label: 'Bold', style: 'BOLD' },
-	{ label: 'Italic', style: 'ITALIC' },
-	{ label: 'Underline', style: 'UNDERLINE' },
-	{ label: 'Monospace', style: 'CODE' },
-]
-
-const InlineStyleControls: React.FC<StyleControlsTypes> = ({ editorState, onToggle }) => {
-	const currentStyle = editorState.getCurrentInlineStyle()
-
-	return (
-		<div className="RichEditor-controls">
-			{INLINE_STYLES.map((type) => (
-				<StyleButton
-					key={type.label}
-					active={currentStyle.has(type.style)}
-					label={type.label}
-					onToggle={onToggle}
-					style={type.style}
-				/>
-			))}
-		</div>
-	)
-}
-
-const BLOCK_TYPES = [
-	{ label: 'H1', style: 'header-one' },
-	{ label: 'H2', style: 'header-two' },
-	{ label: 'H3', style: 'header-three' },
-	{ label: 'Math', style: 'math' },
-	{ label: 'Image', style: 'image-block' },
-	// {label: 'H4', style: 'header-four'},
-	// {label: 'H5', style: 'header-five'},
-	// {label: 'H6', style: 'header-six'},
-	// {label: 'Blockquote', style: 'blockquote'},
-	{ label: 'UL', style: 'unordered-list-item' },
-	// {label: 'OL', style: 'ordered-list-item'},
-	// {label: 'Code Block', style: 'code-block'},
-]
-
-const BlockStyleControls = (props: StyleControlsTypes) => {
-	const { editorState } = props
-	const selection = editorState.getSelection()
-	const blockType = editorState
-		.getCurrentContent()
-		.getBlockForKey(selection.getStartKey())
-		.getType()
-
-	return (
-		<div className="RichEditor-controls">
-			{BLOCK_TYPES.map((type) => (
-				<StyleButton
-					key={type.label}
-					active={type.style === blockType}
-					label={type.label}
-					onToggle={props.onToggle}
-					style={type.style}
-				/>
-			))}
-		</div>
-	)
-}
-
-// Custom overrides for "code" style.
-const styleMap = {
-	CODE: {
-		backgroundColor: 'rgba(0, 0, 0, 0.05)',
-		fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-		fontSize: 16,
-		padding: 2,
-	},
-}
-
-function getBlockStyle(block: ContentBlock): null | string {
-	switch (block.getType()) {
-		case 'blockquote':
-			return 'RichEditor-blockquote'
-		default:
-			return null
-	}
 }
 
 const RichTextEditor: React.FC<RichTextEditorTypes> = ({ login, store }) => {
