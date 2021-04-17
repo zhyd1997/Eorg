cfa19573a6975c21723126dbd3315cc8ed2340c0
007tom <zhyd007@gmail.com>
2021-04-17 15:48:31 +0800

    added CSS box model
    - ncu -u

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
 
 if (process.env.NODE_ENV === "development") {
     const { worker } = require("./mocks/browser");
