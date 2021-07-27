9ce93e48ce50956d7a99b5beb99cee00fab23d57
007tom <zhyd007@gmail.com>
2021-07-27 16:19:56 +0800

    feat: added msw for local dev mocking API

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,16 @@
+import { rest } from "msw";
+import { baseUrl } from "../components/baseUrl";
+
+export const handlers = [
+  rest.post(`${baseUrl}users/signup`, (req, res, ctx) => {
+    return res(ctx.status(201), ctx.json({ success: true }));
+  }),
+
+  rest.post(`${baseUrl}users/login`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ success: true, token: "test" }));
+  }),
+
+  rest.get(`${baseUrl}users/logout`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
