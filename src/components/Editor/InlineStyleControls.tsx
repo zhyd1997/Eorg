9a6f01a31bf6d441376cf9879c73cb3983379cde
Yadong Zhang <zhyd007@gmail.com>
2021-07-30 14:11:25 +0800

    build: updated browserlist (#51)
    + npx browserslist@latest --update-db
    
    Note:  I am not sure change `Monospace` to `Code` will work when using [convertFromRaw](https://draftjs.org/docs/api-reference-data-conversion#convertfromraw) API.

--- src/components/Editor/InlineStyleControls.tsx
+++ src/components/Editor/InlineStyleControls.tsx
@@ -6,7 +6,7 @@ const INLINE_STYLES = [
   { label: "Bold", style: "BOLD" },
   { label: "Italic", style: "ITALIC" },
   { label: "Underline", style: "UNDERLINE" },
-  { label: "Monospace", style: "CODE" },
+  { label: "Code", style: "CODE" },
 ];
 
 export const InlineStyleControls = ({
