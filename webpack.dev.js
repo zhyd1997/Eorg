336684d7a23d1f219520c537a33f952b2e040d8b
Yadong Zhang <zhyd007@gmail.com>
2021-10-03 11:41:53 +0800

    fix: HMR (#66)
    bump all deps to latest versions.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,16 @@
 const { merge } = require("webpack-merge");
+const path = require("path");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./public",
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    hot: true,
   },
   optimization: {
     moduleIds: "deterministic",
